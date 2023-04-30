749dbd1a32705b8b6352bdcffe144976b1300079
Mikhail Akhmedzyanov <mikhail.akhmedzyanov@gmail.com>
2023-04-30 14:17:42 +0300

    fix: fix bugs with method of shifts and caps keys and refactor this functions

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let btnKeyboard1 = document.querySelector("#button1");
 let btnKeyboard2 = document.querySelector("#button2");
 let btnKeyboard3 = document.querySelector("#button3");
 
+//TODO: create a vertual keyboard also from scripts
 let virtualKeyboard = document.querySelector('.visual-keyboard');
 
 // const keyLayout = [
@@ -16,31 +17,70 @@ let virtualKeyboard = document.querySelector('.visual-keyboard');
 
 class KeyButton {
 
-    constructor(type, en, ru, lang) {
-      this.type = type;
-      this.en = en;
-      this.ru = ru;
-      this.value = this.setBtnValue(lang).value;
-      this.shiftValue = this.setBtnValue(lang).shiftValue;
+    constructor(obj, lang) {
+      this.type = obj.type;
+      this.lang = lang;
+      this.key = obj.element;
+      this.mainLang = this.setButtonLang();
+      if (this.type !== 'func') {
+        this.setButtonValue();
+      } else {
+        this.value = this.key.en.value;
+        this.code = obj.code;
+      }
     }
 
-    setBtnValue(lang) {
+    setButtonLang() {
       let result;
-      switch (lang) {
+      switch (this.lang) {
         case 'en':
-          result = this.en;
+          result = this.key.en;
           break;
         case 'ru':
-          result = this.ru;
+          result = this.key.ru == undefined ? this.key.en : this.key.ru;
           break;
         default:
           throw new Error(`Can't recognize the language`);
-          break;
       }
       return result;
     }
 
-    getSymbValue(){
+    setButtonValue() {
+      this.value = this.mainLang.value;
+      this.shiftValue = this.mainLang.shiftValue;
+    }
+
+    toggleValue(){
+      if (this.type !== 'func') {
+        for (let lang in this.key) {
+          let buffer = this.key[lang].value;
+          this.key[lang].value = this.key[lang].shiftValue;
+          this.key[lang].shiftValue = buffer;
+        }
+      }
+      this.mainLang = this.setButtonLang();
+      this.setButtonValue();
+      return this;
+    }
+
+    toggleLanguage(){
+      switch (this.lang) {
+        case 'en':
+          this.mainLang = this.key.ru == undefined ? this.key.en : this.key.ru;
+          this.lang = 'ru';
+          break;
+        case 'ru':
+          this.mainLang = this.key.en;
+          this.lang = 'en';
+          break;
+        default:
+          throw new Error(`Can't recognize the language`);
+      }
+      this.setButtonValue();
+      return this;
+    }
+    
+    returnValue(){
       return this.value;
     }
 }
@@ -53,6 +93,10 @@ class KeyBoard {
     //elem.onclick = this.onClick.bind(this);
     this.keysList = {};
     this.language = 'en';
+    this.isShiftPressed = false;
+    this.isCtrlPressed = false;
+    this.isAltPressed = false;
+    this.isCapsPressed = false;
   }
 
   onClick(event){
@@ -67,10 +111,8 @@ class KeyBoard {
     for (let key in obj) {
       //TODO: check local storage and get lang value; try catch and set default;
       let lang = 'en';
-      let enSet = obj[key].element.en;
-      let ruSet = obj[key].element.ru == undefined ? obj[key].element.en : obj[key].element.ru;
-      let type = obj[key].type;
-      let keyButton = new KeyButton(type, enSet, ruSet, lang);
+      let element = obj[key];
+      let keyButton = new KeyButton(element, lang);
       const htmlButton = this.createKeyButton(key, keyButton.value);
       this.keysList[key] = keyButton;
       this.elem.appendChild(htmlButton);
@@ -84,39 +126,157 @@ class KeyBoard {
     htmlButton.className = "key-button";
     htmlButton.id = `${id}`;
     htmlButton.innerText = `${value}`;
-    htmlButton.addEventListener('click', () => {
-      this.buttonHandler(htmlButton)
+    htmlButton.addEventListener('mousedown', () => {
+      this.buttonMouseDownHandler(htmlButton)
+      
+    });
+    htmlButton.addEventListener('mouseup', () => {
+     
+      this.buttonMouseUpHandler(htmlButton)
     });
     return htmlButton;
   }
   
+  setActiveState(active, elem){
+    let htmlBtn;
+    switch (active) {
+      case true:
+        htmlBtn = this.elem.querySelector(`#${elem}`);
+        htmlBtn.classList.add("key-button-active");
+        break;
+      case false:
+        htmlBtn = this.elem.querySelector(`#${elem}`);
+        htmlBtn.classList.remove("key-button-active");
+        break;
+      
+      default:
+        throw new Error(`can't set active state`);
+    }
+  }
+
   changeLang() {
     for (let k in this.keysList) {
       let btn;
-      switch (this.language) {
-        case 'en':
-          btn = document.getElementById(k);
-          btn.innerText = ``;
-          try {   btn.innerText = `${this.keysList[k].ru.value}` } 
-          catch { btn.innerText = `${this.keysList[k].en.value}` }
-          break;
-
-        case 'ru':
-          btn = document.getElementById(k);
-          btn.innerText = ``;
-          try {   btn.innerText = `${this.keysList[k].en.value}` }
-          catch { btn.innerText = `${this.keysList[k].en.value}` }
-        default:
-          break;
+      let keyButton = this.keysList[k];
+      if (keyButton.type !== 'func') {
+        btn = document.getElementById(k);
+        btn.innerText = ``;
+        keyButton.toggleLanguage(this.language);
+        btn.innerText = `${keyButton.returnValue()}`;
       }
     }
-    this.language = this.language === 'en' ? 'ru' : 'en';
+    this.language = (this.language === 'en' ? 'ru' : 'en');
+    
   }
 
+  shiftButtons(isShift){
+    for (let k in this.keysList) {
+      let btn;
+      let keyButton = this.keysList[k];
+      keyButton.toggleValue();
+      if (keyButton.type !== 'func') {
+        btn = document.getElementById(k);
+        btn.innerText = ``;
+        btn.innerText = `${keyButton.returnValue()}`;
+      }
+    }
+  }
+
+  capsButtons(isCaps){
+    for (let k in this.keysList) {
+      let btn;
+      let keyButton = this.keysList[k];
+      
+      if (this.keysList[k].type === 'char') {
+        keyButton.toggleValue();
+        btn = document.getElementById(k);
+        btn.innerText = ``;
+        btn.innerText = `${keyButton.returnValue()}`;
+      }
+    }
+  }
   
-  buttonHandler(htmlButton){
+  buttonMouseUpHandler(htmlButton){
     if (`${htmlButton.id}` in this.keysList) {
-      console.log(`type = ${this.keysList[htmlButton.id].type} value = '${htmlButton.innerText}'`);
+      let entiresBtn = this.keysList[htmlButton.id]
+      //console.log(`type = ${entiresBtn.type} html value = '${htmlButton.innerText}' btn value = '${entiresBtn.value}' code button = ${entiresBtn.code}`);
+      this.buttonKeyUpHandler(entiresBtn.code);
+    } 
+  }
+  buttonMouseDownHandler(htmlButton){
+    if (`${htmlButton.id}` in this.keysList) {
+      let entiresBtn = this.keysList[htmlButton.id]
+      console.log(`type = ${entiresBtn.type} html value = '${htmlButton.innerText}' btn value = '${entiresBtn.value}' code button = ${entiresBtn.code}`);
+      this.buttonPressDownHandler(entiresBtn.code);
+    } 
+  }
+
+  buttonKeyUpHandler(keyButton) {
+    this.buttonPressUpHandler(keyButton);
+  }
+
+  buttonKeyDownHandler(keyButton) {
+    this.buttonPressDownHandler(keyButton);
+  }
+
+  buttonPressDownHandler(btn){
+    if (`${btn}` in this.keysList) {
+
+      if (!this.isShiftPressed && (btn === "ShiftLeft" || btn === "ShiftRight")) {
+        //console.log(`Shift has pressed! ${btn}`);
+        this.isShiftPressed = true;
+        this.setActiveState(this.isShiftPressed, btn);
+        this.shiftButtons(this.isShiftPressed);
+      }
+      else if (!this.isCtrlPressed && (btn === "ControlLeft" || btn === "ControlRight")) {
+        //console.log(`Ctrl has pressed! ${btn}`);
+        this.isCtrlPressed = true;
+        this.setActiveState(this.isCtrlPressed, btn);
+      }
+      else if (!this.isAltPressed && (btn === "AltRight" || btn === "AltLeft")) {
+        //console.log(`Alt has pressed! ${btn}`);
+        this.isAltPressed = true;
+        this.setActiveState(this.isAltPressed, btn);
+      }
+      else if(btn === "CapsLock") {
+        console.log(`CAPS has pressed! ${btn}`);
+        
+        this.isCapsPressed = !this.isCapsPressed;
+        this.capsButtons(this.isCapsPressed);
+        this.setActiveState(this.isCapsPressed, btn);
+      }
+      //Default:
+      else {
+        this.setActiveState(true, btn);
+      }
+
+      if (this.isCtrlPressed && this.isAltPressed) {
+        console.log(`change lang! ${btn}`);
+        this.changeLang();
+      }
+      //console.log(`type = ${this.keysList[keyButton].type} value = '${this.keysList[keyButton].value}'`);
+    }
+  }
+
+  buttonPressUpHandler(btn){
+    if (`${btn}` in this.keysList) {
+      
+      if (this.isShiftPressed && (btn === "ShiftLeft" || btn === "ShiftRight")) {
+        this.isShiftPressed = false;
+        this.shiftButtons(this.isShiftPressed);
+        this.setActiveState(this.isShiftPressed, btn);
+      }
+      else if (this.isCtrlPressed && (btn === "ControlLeft" || btn === "ControlRight")) {
+        this.isCtrlPressed = false;
+        this.setActiveState(this.isCtrlPressed, btn);
+      }
+      else if (this.isAltPressed && (btn === "AltRight" || btn === "AltLeft")) {
+        this.isAltPressed = false;
+        this.setActiveState(this.isAltPressed, btn);
+      }
+      else if(btn !== "CapsLock") {
+        this.setActiveState(false, btn);
+      }
     }
   }
 }
@@ -134,42 +294,19 @@ fetch('/keys.json')
 
 
 // Phisycal Keyboard handler to special buttons
-let isShiftPressed = false;
-let isCtrlPressed = false;
-let isAltPressed = false;
+// let isShiftPressed = false;
+// let isCtrlPressed = false;
+// let isAltPressed = false;
 
 document.addEventListener('keydown', function(event) { 
   let code = event.code;
-  if (!isShiftPressed && (code === "ShiftLeft" || code === "ShiftRight")) {
-    console.log(`Shift has pressed! ${event.code}`);
-    isShiftPressed = true;
-  }
-  if (!isCtrlPressed && (code === "ControlLeft" || code === "ControlRight")) {
-    console.log(`Ctrl has pressed! ${event.code}`);
-    isCtrlPressed = true;
-  }
-  if (!isAltPressed && (code === "AltRight" || code === "AltLeft")) {
-    console.log(`Alt has pressed! ${event.code}`);
-    isAltPressed = true;
-  }
-  if (isShiftPressed && isAltPressed) {
-    keyBoard.changeLang();
-  }
-  console.log(`pressed btn = ${code}`);
-  
+  keyBoard.buttonKeyDownHandler(code);
+  console.log(`!! pressed btn = ${code}`);
 });
 
 document.addEventListener('keyup', function(event) {
   let code = event.code;
-  if (isShiftPressed && (code === "ShiftLeft" || code === "ShiftRight")) {
-    isShiftPressed = false;
-  }
-  if (isCtrlPressed && (code === "ControlLeft" || code === "ControlRight")) {
-    isCtrlPressed = false;
-  }
-  if (isAltPressed && (code === "AltRight" || code === "AltLeft")) {
-    isAltPressed = false;
-  }
+  keyBoard.buttonKeyUpHandler(code);
 
 });
 
