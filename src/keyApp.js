5a913f5f3abb26616c0e6f95e1750acaf20d14b6
Mikhail Akhmedzyanov <mikhail.akhmedzyanov@gmail.com>
2023-05-01 23:44:08 +0300

    feat: add css rules for better design

--- src/keyApp.js
+++ src/keyApp.js
@@ -1,9 +1,9 @@
 import KEYS from './keysData.js';
 import KeyBoard from './keyBoard.js';
 
-
 // TODO: create a vertual keyboard also from scripts
-const body = document.querySelector("body");
+function app() {
+  const body = document.querySelector('body');
   const wrapper = document.createElement('section');
   wrapper.className = 'wrapper';
   body.appendChild(wrapper);
@@ -13,6 +13,15 @@ const body = document.querySelector("body");
   const virtualKeyboard = document.createElement('div');
   virtualKeyboard.className = 'virtual-keyboard';
   wrapper.appendChild(virtualKeyboard);
+  const instruction = document.createElement('div');
+  instruction.className = 'instruction';
+  wrapper.appendChild(instruction);
+  let inst = document.createElement('p');
+  inst.innerText = 'Клавиатура создана в операционной системе Windows';
+  instruction.appendChild(inst);
+  inst = document.createElement('p');
+  inst.innerText = 'Для переключения языка комбинация: левыe ctrl + alt';
+  instruction.appendChild(inst);
   let language = localStorage.getItem('lang');
   language = language === null ? 'en' : language;
   const keyBoard = new KeyBoard(virtualKeyboard, textArea, language);
@@ -38,3 +47,6 @@ const body = document.querySelector("body");
     const { code } = event;
     keyBoard.buttonKeyUpHandler(code);
   });
+}
+
+app();
--- src/keyBoard.js
+++ src/keyBoard.js
@@ -115,13 +115,35 @@ export default class KeyBoard {
       let btn;
       const keyButton = objKeys[index][1];
       const key = objKeys[index][0];
-      const result = keyButton.returnValue().match(/[a-zа-яё]/ig);
+      const result = keyButton.value.match(/[a-zа-яё]/ig);
       if (this.keysList[key].type === 'char' && result) {
         keyButton.toggleValue();
         btn = document.getElementById(key);
         btn.innerText = '';
         btn.innerText = `${keyButton.returnValue()}`;
       }
+    }
+  }
+
+  addChar(char) {
+    const outputCharArr = this.input.split('');
+    outputCharArr.splice(this.cursor, 0, char);
+    this.input = outputCharArr.join('');
+    setTimeout(() => {
+      this.cursor += 1;
+      this.textArea.value = this.input;
+    }, 10);
+  }
+
+  deleteChar(index) {
+    if (this.cursor > 0 && this.cursor <= this.input.length) {
+      const outputCharArr = this.input.split('');
+      outputCharArr.splice(this.cursor + index, 1);
+      this.input = outputCharArr.join('');
+      setTimeout(() => {
+        this.cursor += index;
+        this.textArea.value = this.input;
+      }, 10);
     }
   }
 
@@ -154,7 +176,7 @@ export default class KeyBoard {
   buttonPressDownHandler(btn) {
     this.textArea.focus();
     if (`${btn}` in this.keysList) { // Functionality key
-      let btnType = this.keysList[btn].type;
+      const btnType = this.keysList[btn].type;
       if (btnType === 'func') {
         if (!this.isShiftPressed && (btn === 'ShiftLeft' || btn === 'ShiftRight')) {
           this.isShiftPressed = true;
@@ -182,7 +204,7 @@ export default class KeyBoard {
           this.deleteChar(0);
         }
       } else if (btnType !== 'func'
-              && btnType !== 'lang') { // Default key:
+        && btnType !== 'lang') { // Default key:
         this.setActiveState(true, btn);
         this.addChar(this.keysList[btn].value);
         console.log(this.input);
@@ -190,38 +212,16 @@ export default class KeyBoard {
         this.changeLang();
       }
 
-      if (this.isShiftPressed && this.isAltPressed && !this.hasChangedLanguage) {
+      if (this.isCtrlPressed && this.isAltPressed && !this.hasChangedLanguage) {
         this.hasChangedLanguage = true;
         this.changeLang();
       }
     }
   }
 
-  addChar(char) {
-    const outputCharArr = this.input.split('');
-    outputCharArr.splice(this.cursor, 0, char);
-    this.input = outputCharArr.join('');
-    setTimeout(() => {
-      this.cursor += 1;
-      this.textArea.value = this.input;
-    }, 10);
-  }
-
-  deleteChar(index) {
-    if (this.cursor > 0 && this.cursor <= this.input.length) {
-      const outputCharArr = this.input.split('');
-      outputCharArr.splice(this.cursor + index, 1);
-      this.input = outputCharArr.join('');
-      setTimeout(() => {
-        this.cursor += index;
-        this.textArea.value = this.input;
-      }, 10);
-    }
-  }
-
   buttonPressUpHandler(btn) {
     if (`${btn}` in this.keysList) {
-      if (this.isShiftPressed && this.isAltPressed && this.hasChangedLanguage) {
+      if (this.isCtrlPressed && this.isAltPressed && this.hasChangedLanguage) {
         this.hasChangedLanguage = false;
       }
       if (this.isShiftPressed && (btn === 'ShiftLeft' || btn === 'ShiftRight')) {
